0502f2e9e8416ade0d594a3a4966b6e1ba2a5b33
SSR707 <samandarshavkatov07@gmail.com>
2025-05-01 18:31:11 +0500

    feat: complete ERP project

--- src/config/AxiosInstance.tsx
+++ src/config/AxiosInstance.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookie from "js-cookie";
 
 export const instance = axios.create({
-  baseURL: "https://api.admin.bekzodjon.uz/api/docs",
+  baseURL: "https://api.admin.bekzodjon.uz/api/v1",
 });
 
 instance.interceptors.request.use((config) => {
